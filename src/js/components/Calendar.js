2d2c7d7779aa581b6386d439af1933bc856e5a69
Benedikt Gregor <runbgn@gmail.com>
2020-10-03 13:20:22 +0200

    improv jsx markup

--- src/js/components/Calendar.js
+++ src/js/components/Calendar.js
@@ -1,33 +1,64 @@
 import React from 'react';
+import { pad } from '../helpers/helpers.js';
 
 const Calendar = (props) => {
   const { inputDate } = props;
-  const dateString = `${inputDate.getFullYear()} ${inputDate.getMonth() + 1}`;
+  const dateString = `${inputDate.getFullYear()}-${pad(
+    inputDate.getMonth() + 1
+  )}-${pad(inputDate.getDate())}`;
   console.log(inputDate.toString());
 
   return (
     <div className="calendar">
       <h1>Calendar</h1>
-      <p>{dateString}</p>
-      <TableOuter />
+      <p>
+        <code>inputDate: {dateString}</code>
+      </p>
+
+      <table>
+        <CalendarHead />
+        <CalendarBody inputDate={inputDate} />
+      </table>
     </div>
   );
 };
 
 export default Calendar;
 
-function TableOuter() {
+function CalendarHead() {
   const dayNames = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
   let cells = [];
   for (let i = 0; i < 7; i++) {
-    cells.push(<td>{dayNames[i]}</td>);
+    cells.push(<td key={`weekday-headcell-${i}`}>{dayNames[i]}</td>);
   }
 
   return (
-    <table>
-      <tbody>
-        <tr>{cells}</tr>
-      </tbody>
-    </table>
+    <thead>
+      <tr>{cells}</tr>
+    </thead>
   );
+}
+
+function CalendarBody(inputDate) {
+  // getFirstDay(inputDate);
+
+  let rows = [];
+
+  for (let i = 0; i < 6; i++) {
+    let cells = [];
+
+    for (let i = 0; i < 7; i++) {
+      cells.push(<td key={`weekday-bodycell-${i}`}></td>);
+    }
+
+    rows.push(<tr key={`weekday-bodyrow-${i}`}>{cells}</tr>);
+  }
+  return <tbody>{rows}</tbody>;
+}
+
+// Get First (Week-)Day
+function getFirstDay(date) {
+  let tempDate = new Date(date.getTime());
+  tempDate.setDate(1);
+  console.log('getFWD', tempDate, (tempDate.getDay() + 6) % 7);
 }
---
+++ src/js/helpers/helpers.js
@@ -0,0 +1,20 @@
+export function ISODateString(d) {
+  return (
+    d.getUTCFullYear() +
+    '-' +
+    pad(d.getUTCMonth() + 1) +
+    '-' +
+    pad(d.getUTCDate()) +
+    'T' +
+    pad(d.getUTCHours()) +
+    ':' +
+    pad(d.getUTCMinutes()) +
+    ':' +
+    pad(d.getUTCSeconds()) +
+    'Z'
+  );
+}
+
+export function pad(n) {
+  return n < 10 ? '0' + n : n;
+}
