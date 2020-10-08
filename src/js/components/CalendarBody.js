57ac853165a1d69407e5ca4be95b6f04892519b5
Benedikt Gregor <runbgn@gmail.com>
2020-10-08 15:21:31 +0200

    append dates to calCells

--- src/js/components/CalendarBody.js
+++ src/js/components/CalendarBody.js
@@ -1,3 +1,4 @@
+import { doc } from 'prettier';
 import React from 'react';
 import {
   getFirstDay,
@@ -6,17 +7,61 @@ import {
 } from '../helpers/helpers.js';
 
 function CalendarBody({ inputDate, records }) {
+  const tbody = React.useRef();
   const firstDay = getFirstDay(inputDate);
   const daysInMonth = getDaysInMonth(inputDate);
-  const currentDate = new Date();
+  const todayDate = new Date();
+  const todayShortString = getShortIsoString(todayDate);
+
+  console.log(records);
+
   let date = 1;
   let rows = [];
 
   React.useEffect(() => {
-    // get records for inputDate Month
-    console.log(records);
-    // append data to cells
+    const allDateCells = document.querySelectorAll('[data-date-string]');
+    const todayCell = document.querySelector(
+      `[data-date-string="${todayShortString}"]`
+    );
+
+    if (todayCell) todayCell.dataset.today = '';
+
+    function appendDates() {
+      allDateCells.forEach((cell) => {
+        // !TODO: Account for 2 more more entries
+        // matched Records forEach append to cell?
+        const matchedRecord = records.filter((record) => {
+          const isMatch =
+            getShortIsoString(new Date(record.begin)) == cell.dataset.dateString
+              ? record
+              : null;
+
+          if (isMatch) {
+            //
+            cell
+              .querySelector('[data-records]')
+              .insertAdjacentHTML(
+                'beforeend',
+                `<small style="font-size: .5rem; color: rebeccapurple"> ${record.id}</small>`
+              );
+          }
+        });
+      });
+    }
+
+    appendDates();
+
+    // Cleanup
+    return () => {
+      tbody.current
+        .querySelectorAll('[data-records]')
+        .forEach((el) => (el.innerHTML = ``));
+
+      allDateCells.forEach((cell) => cell.removeAttribute('data-today'));
+    };
   });
+
+  const currentDate = new Date(inputDate);
 
   for (let i = 0; i < 6; i++) {
     let cells = [];
@@ -32,12 +77,11 @@ function CalendarBody({ inputDate, records }) {
       } else {
         currentDate.setDate(date);
         cells.push(
-          <td
-            data-date-string={getShortIsoString(currentDate)}
+          <CalendarCell
+            dateString={getShortIsoString(currentDate)}
+            date={date}
             key={`weekday-bodycell-${j}`}
-          >
-            {date}
-          </td>
+          />
         );
         date++;
       }
@@ -45,7 +89,17 @@ function CalendarBody({ inputDate, records }) {
 
     rows.push(<tr key={`weekday-bodyrow-${i}`}>{cells}</tr>);
   }
-  return <tbody>{rows}</tbody>;
+
+  return <tbody ref={tbody}>{rows}</tbody>;
 }
 
 export default CalendarBody;
+
+function CalendarCell({ dateString, date }) {
+  return (
+    <td data-date-string={dateString}>
+      <span>{date}</span>
+      <span data-records></span>
+    </td>
+  );
+}
