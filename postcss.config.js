daf4ecf8541dc031035655af2a1df53b9e0a0012
Benedikt Gregor <runbgn@gmail.com>
2020-10-28 13:25:01 +0100

    stylelint prettier cfg

--- postcss.config.js
+++ postcss.config.js
@@ -3,13 +3,13 @@ module.exports = (ctx) => ({
   plugins: {
     // 'postcss-import': { root: ctx.file.dirname },
     'postcss-mixins': {},
+    // 'postcss-simple-vars': {},
     'postcss-size': {},
     'postcss-preset-env': {
       // browsers: 'last 2 versions',
       stage: 0, // 0: polyfill all, 4: polyfill none, but polyfill the following features: { 'nesting-rules': true}
       features: {
-        'nesting-rules': true,
-        'custom-selectors': true
+        'nesting-rules': true
       }
     },
 
