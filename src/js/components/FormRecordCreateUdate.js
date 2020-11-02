077ba0816fdcf24eeca9356ab8a9ce0dd0c490c4
Benedikt Gregor <runbgn@gmail.com>
2020-11-02 11:06:54 +0100

    remove unused import

--- src/js/components/FormRecordCreateUdate.js
+++ src/js/components/FormRecordCreateUdate.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import FormRecord from './FormRecord';
-import { formatDate, getTimeOfDate } from '../utils/helpers';
+import { getTimeOfDate } from '../utils/helpers';
 
 export function FormRecordCreate({
   inputDate,
