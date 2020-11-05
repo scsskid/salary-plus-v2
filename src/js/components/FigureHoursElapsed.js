04c411abe1aec9ac57b558af0c85160a814b22e0
Benedikt Gregor <runbgn@gmail.com>
2020-11-05 17:32:54 +0100

    2 decimals

--- src/js/components/FigureHoursElapsed.js
+++ src/js/components/FigureHoursElapsed.js
@@ -3,5 +3,5 @@ import { addRecordHoursElapsed } from '../utils/reporting-fns';
 import { round } from '../utils/helpers';
 
 export default function FigureHoursElapsed({ records }) {
-  return <span>{round(records.reduce(addRecordHoursElapsed, 0), 1)}h</span>;
+  return <span>{round(records.reduce(addRecordHoursElapsed, 0), 2)}h</span>;
 }
