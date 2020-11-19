ff2174aa7b6f7b3506cebdf6bdcc0260d3089adc
Benedikt Gregor <runbgn@gmail.com>
2020-11-19 10:59:58 +0100

    return null if dbug option not set

--- src/js/components/LogToScreen.js
+++ src/js/components/LogToScreen.js
@@ -1,9 +1,7 @@
 import * as React from 'react';
 
 export default function LogToScreen({ object, settings }) {
-  return (
-    settings.showDebugInfo && (
-      <pre style={{ fontSize: '.6rem' }}>{JSON.stringify(object, null, 2)}</pre>
-    )
-  );
+  return settings.showDebugInfo ? (
+    <pre style={{ fontSize: '.6rem' }}>{JSON.stringify(object, null, 2)}</pre>
+  ) : null;
 }
