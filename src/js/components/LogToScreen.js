f3bbc5d14d7b0add24f45b30dcc987737a4d82fe
Benedikt Gregor <runbgn@gmail.com>
2020-11-20 20:12:26 +0100

    print title

--- src/js/components/LogToScreen.js
+++ src/js/components/LogToScreen.js
@@ -1,7 +1,14 @@
 import * as React from 'react';
 
-export default function LogToScreen({ object = {}, settings = {} }) {
+export default function LogToScreen({
+  title = '',
+  object = {},
+  settings = {}
+}) {
   return settings.showDebugInfo ? (
-    <pre style={{ fontSize: '.6rem' }}>{JSON.stringify(object, null, 2)}</pre>
+    <>
+      {title && <pre style={{ fontSize: '.6rem' }}>{title}</pre>}
+      <pre style={{ fontSize: '.6rem' }}>{JSON.stringify(object, null, 2)}</pre>
+    </>
   ) : null;
 }
