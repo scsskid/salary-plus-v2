897fe51e2b821f7dcaf90ae99dba57fadc34283e
Benedikt Gregor <runbgn@gmail.com>
2020-11-26 15:35:15 +0100

    localization

--- src/js/components/InputDateDisplay.js
+++ src/js/components/InputDateDisplay.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 export default function InputDateDisplay({ inputDate, settings }) {
   return (
     <div className="input-date-display">
-      {inputDate.toLocaleDateString(undefined, {
+      {inputDate.toLocaleDateString(settings.language, {
         month: 'long',
         timeZone: settings.timeZone
       })}{' '}
