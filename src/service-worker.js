16bbdcdf4b2359de6d1c9ed25d8dd3b8d6f61ba4
scsskid <runbgn@gmail.com>
2020-12-28 19:13:29 +0100

    handle navigation (SPA Fallback)

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,10 @@
-import { registerRoute } from 'workbox-routing';
+import { NavigationRoute, registerRoute } from 'workbox-routing';
 import { StaleWhileRevalidate } from 'workbox-strategies';
-import { precacheAndRoute } from 'workbox-precaching';
+import { createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching';
 
 registerRoute(new RegExp('/(.*)'), new StaleWhileRevalidate());
 precacheAndRoute(self.__WB_MANIFEST);
+
+const handler = createHandlerBoundToURL('/index.html');
+const navigationRoute = new NavigationRoute(handler);
+registerRoute(navigationRoute);
