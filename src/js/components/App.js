410e5f546474e6ede8883e162ec0d61202b770d4
scsskid <runbgn@gmail.com>
2020-12-30 20:28:27 +0100

    try get dismiss pormpt while preserving update functionality (wip)

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -40,71 +40,52 @@ import { Workbox, messageSW } from 'workbox-window';
 
 function SWWrapper() {
   const [showPrompt, setShowPrompt] = React.useState(false);
+
   const wb = new Workbox('/service-worker.js');
 
   let registration;
 
-  const handlers = {
-    // `event.wasWaitingBeforeRegister` will be false if this is
-    // the first time the updated service worker is waiting.
-    // When `event.wasWaitingBeforeRegister` is true, a previously
-    // updated service worker is stil l waiting.
-    // You may want to customize the UI prompt accordingly.
+  const onAccept = async () => {
+    wb.addEventListener('controlling', (event) => {
+      window.location.reload();
+    });
 
-    // Assumes your app has some sort of prompt UI element
-    // that a user can either accept or reject.
-
-    onAccept: async () => {
-      // Assuming the user accepted the update, set up a listener
-      // that will reload the page as soon as the previously waiting
-      // service worker has taken control.
-      wb.addEventListener('controlling', (event) => {
-        window.location.reload();
-      });
-
-      if (registration && registration.waiting) {
-        // Send a message to the waiting service worker,
-        // instructing it to activate.
-        // Note: for this to work, you have to add a message
-        // listener in your service worker. See below.
-        messageSW(registration.waiting, { type: 'SKIP_WAITING' });
-      }
-    },
-
-    onReject: () => {
-      setShowPrompt(false);
+    if (registration && registration.waiting) {
+      messageSW(registration.waiting, { type: 'SKIP_WAITING' });
     }
   };
 
   React.useEffect(() => {
-    // Add an event listener to detect when the registered
-    // service worker has installed but is waiting to activate.
-    wb.addEventListener('waiting', handleSWWaiting);
-    wb.addEventListener('externalwaiting', handleSWWaiting);
+    wb.addEventListener('waiting', () => {
+      setShowPrompt(true);
+    });
+    wb.addEventListener('externalwaiting', () => {
+      setShowPrompt(true);
+    });
 
     wb.register().then((r) => {
       console.log(r);
       registration = r;
     });
-  });
+  }, []);
 
-  function handleSWWaiting() {
-    setShowPrompt(true);
-  }
-
-  function createUIPrompt(onAccept, onReject) {
-    return (
+  return (
+    showPrompt && (
       <div>
-        <h1>Update Available</h1>
+        <h1>5</h1>
         <p>
-          <button onClick={onReject}>Not now</button>{' '}
+          <button
+            onClick={() => {
+              setShowPrompt(false);
+            }}
+          >
+            Not now
+          </button>{' '}
           <button onClick={onAccept}>Install</button>
         </p>
       </div>
-    );
-  }
-
-  return showPrompt && createUIPrompt(handlers.onAccept, handlers.onReject);
+    )
+  );
 }
 
 export default function App() {
