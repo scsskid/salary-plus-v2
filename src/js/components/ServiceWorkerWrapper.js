a093df893cbe891955b7d3bcc10083d099a623a9
scsskid <runbgn@gmail.com>
2021-01-02 16:06:23 +0100

    remove logging sw registration

--- src/js/components/ServiceWorkerWrapper.js
+++ src/js/components/ServiceWorkerWrapper.js
@@ -33,7 +33,6 @@ export default function ServiceWorkerWrapper() {
   }, []);
 
   wb.register().then((r) => {
-    console.log('wb window: successful registered', r);
     registration = r;
   });
 
